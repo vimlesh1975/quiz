ae000764d4ddd6423a4be36793b5cb1e063e1eb8
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2026-03-29 18:33:08 +0530

    header

--- src/app/page.js
+++ src/app/page.js
@@ -2,25 +2,41 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  const images = Array.from({ length: 10 }, (_, i) => `/image${i + 1}.svg`);
+  const images = [
+    '/IT samples Q&A/Q & A 1a.jpg',
+    '/IT samples Q&A/Q & A 1b.jpg',
+    '/IT samples Q&A/Q & A 2a.jpg',
+    '/IT samples Q&A/Q & A 2b.jpg',
+    '/IT samples Q&A/Q & A 2ba.jpg',
+    '/IT samples Q&A/Q & A 2bb.jpg',
+    '/IT samples Q&A/Q & A 2bc.jpg',
+    '/IT samples Q&A/Q & A 2bd.jpg',
+    '/IT samples Q&A/Q & A 2c.jpg',
+    '/IT samples Q&A/Q & A 3a.jpg',
+    '/IT samples Q&A/Q & A 3b.jpg'
+  ];
 
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Gallery - 10 Images</h1>
         <div className={styles.gallery}>
-          {images.map((image, index) => (
-            <div key={index} className={styles.imageContainer}>
-              <Image
-                src={image}
-                alt={`Image ${index + 1}`}
-                width={400}
-                height={300}
-                priority={index < 3}
-              />
-              <button className={styles.showButton}>Show</button>
-            </div>
-          ))}
+          {images.map((image, index) => {
+            const filename = image.split('/').pop();
+            return (
+              <div key={index} className={styles.imageContainer}>
+                <h3 className={styles.imageTitle}>{filename}</h3>
+                <Image
+                  src={image}
+                  alt={`Image ${index + 1}`}
+                  width={400}
+                  height={300}
+                  priority={index < 3}
+                />
+                <button className={styles.showButton}>Show</button>
+              </div>
+            );
+          })}
         </div>
       </main>
     </div>
